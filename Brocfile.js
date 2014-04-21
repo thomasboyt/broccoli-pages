2f22a3cd4a45fc41eb83ba66aff676e01d2e9cb6
Taras Mankovski <taras@quandl.com>
2014-04-21 09:45:16 -0400

    Upgraded to latest Broccoli

--- Brocfile.js
+++ Brocfile.js
@@ -1,21 +1,25 @@
-var MarkdownPages = require('./lib/index').MarkdownPages,
-    HTMLPages = require('./lib/index').HTMLPages;
+var MarkdownPages = require('./lib/index').MarkdownPages;
+var HTMLPages = require('./lib/index').HTMLPages;
+var pickFiles = require('broccoli-static-compiler');
 
-module.exports = function(broccoli) {
-
-  var example = broccoli.makeTree('example/content'),
-      options = {
-        templates: './example/templates',
-        helpers: './example/helpers',
-        partials: './example/templates/partials',
-        globals: {
-          message: "Hello World!"
-        }
-      };
-
-  example = HTMLPages(example, options);
-  example = MarkdownPages(example, options);
-
-  return example;
-
+var options = {
+  templates: './example/templates',
+  helpers: './example/helpers',
+  partials: './example/templates/partials',
+  globals: {
+    message: "Hello World!"
+  }
 };
+
+var example = 'example';
+
+var content = pickFiles(example, {
+  srcDir: '/content',
+  files: ['**/*.*'],
+  destDir: '/'
+});
+
+var html = HTMLPages(content, options);
+var rendered = MarkdownPages(html, options);
+
+module.exports = rendered;
