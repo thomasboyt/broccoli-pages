ff9978df339328c2a29bd7949c58fa2871756d06
Taras Mankovski <taras@quandl.com>
2014-05-07 08:50:51 -0400

    Reorganized the example and updated README example

--- example/Brocfile.js
+++ example/Brocfile.js
@@ -1,21 +1,3 @@
-# Broccoli Pages Filter
-
-Allows you to generate HTML from Markdown and HTML fragments. You can specify metadata for each page.
-In metadata, you can specify a Handlebars template to be used to wrap this content.
-
-## Example Page
-```markdown
----
-title: Hello World
-description: some text
-template: default.hbs
----
-**Beautiful World**
-```
-
-## Usage
-
-```javascript
 var MarkdownPages = require('broccoli-pages').MarkdownPages;
 var HTMLPages = require('broccoli-pages').HTMLPages;
 var HBSPages = require('broccoli-pages').HBSPages;
@@ -44,6 +26,3 @@ html = MarkdownPages(html, options);
 html = HBSPages(html, options);
 
 module.exports = html;
-```
-
-You can see an example [Brocfile.js](Brocfile.js) and [example](example) directory.
